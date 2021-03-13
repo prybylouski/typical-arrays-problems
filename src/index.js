218fb5ccd24bcd5dbb425b05b4bd0e491d689a15
Uladzimir Prybylouski <eibiologos@gmail.com>
2021-03-13 15:09:22 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array.length === 0 || array == null ) {
+    return 0;
 }
+
+let minNumber = Math.min(...array);
+return minNumber;
+};
+
 
 exports.max = function max (array) {
-  return 0;
+  if (array.length === 0 || array == null) {
+    return 0;
 }
 
+let maxNumber = Math.max(...array);
+return maxNumber;
+};
+
 exports.avg = function avg (array) {
-  return 0;
+  if (array.length === 0 || array == null) {
+    return 0;
 }
+let sum = 0;
+let averageNumber;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+}
+averageNumber = sum / array.length;
+return averageNumber;
+};
