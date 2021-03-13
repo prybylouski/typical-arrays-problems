017ef9d1eea3aabe3511a3d1cd9e80c7a105b665
Uladzimir Prybylouski <eibiologos@gmail.com>
2021-03-13 15:44:57 +0300

    fix:  make correct exception

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
 
 exports.min = function min (array) {
-  if (array.length === 0 || array == null ) {
+  if (array === undefined || array.length === 0) {
     return 0;
+}else{
+  let minNumber = Math.min(...array);
+  return minNumber;
 }
-
-let minNumber = Math.min(...array);
-return minNumber;
 };
 
 
 exports.max = function max (array) {
-  if (array.length === 0 || array == null) {
+  if (array === undefined || array.length === 0) {
     return 0;
+} else {
+  let maxNumber = Math.max(...array);
+  return maxNumber;
 }
+  };
 
-let maxNumber = Math.max(...array);
-return maxNumber;
-};
 
 exports.avg = function avg (array) {
-  if (array.length === 0 || array == null) {
+  if (array === undefined || array.length === 0) {
     return 0;
+} else {
+  let sum = 0;
+  let averageNumber;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+  }
+  averageNumber = sum / array.length;
+  return averageNumber;
 }
-let sum = 0;
-let averageNumber;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-}
-averageNumber = sum / array.length;
-return averageNumber;
 };
